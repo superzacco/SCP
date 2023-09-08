c28f4dce27a2d3b2b1b45dbc7e8e2111cdb0ad10
Zacco <zkmuffingamer@gmail.com>
2023-09-08 17:02:49 -0500

    9/8/23 procedural gen attmepts

--- Unity Project/Assets/Level Gen/Spawner.cs
+++ Unity Project/Assets/Level Gen/Spawner.cs
@@ -1,20 +1,47 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public List<GameObject> roomList;    
+    public List<GameObject> roomList;
+    public GameObject[] spawnPoints; 
 
     private void Start()
     {
-        PickRandomToSpawn();
+        int randFromList = Random.Range(0, roomList.Count); 
+        GameObject room = Instantiate(roomList[randFromList]);
+
+        Invoke("FindSpawnPoints", 2f);
     }
 
-    void PickRandomToSpawn()
+    void FindSpawnPoints()
     {
-        int randFromList = Random.Range(0, roomList.Count - 1); 
-        GameObject firstRoom = Instantiate(roomList[randFromList]);
+        spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+        foreach (GameObject point in spawnPoints)
+        {
+            int randFromList = Random.Range(0, roomList.Count); 
+            GameObject newRoom = Instantiate(roomList[randFromList], point.transform);
+            // CheckForOverlap(newRoom);
+        }
+
+        Invoke("FindSpawnPoints", 1f);
     }
-    //Test
+
+    // public void CheckForOverlap(GameObject newRoom)
+    // {
+    //     Collider newRoomBounds = newRoom.GetComponent<Collider>();
+
+    //     if (newRoomBounds.bounds.Intersects(newRoomBounds.bounds))
+    //     {
+    //         newRoom.transform.Rotate(0, 90, 0);
+    //         CheckForOverlap(newRoom);
+    //     }
+    //     else
+    //     {
+    //         //Continue
+    //     }
+        
+    // }
 }
